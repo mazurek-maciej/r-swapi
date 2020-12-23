92bd6d9426bd98149f0381ca29753a2eedb59034
Maciej Mazurek <maciej.mazurek.prv@gmail.com>
2020-12-23 20:12:57 +0100

    Remove unused clear state types and reducers

--- src/store/starshipsCards/actions/types.ts
+++ src/store/starshipsCards/actions/types.ts
@@ -3,8 +3,7 @@ import { Starship } from "../../starships/models/Starship";
 
 export enum ACTION_TYPE {
   STORE_STARSHIPS_CARDS = 'STORE_STARSHIPS_CARDS',
-  STORE_ERROR_STARSHIPS_CARDS = 'STORE_ERROR_STARSHIPS_CARDS',
-  CLEAR_STARSHIPS_CARDS = 'CLEAR_STARSHIPS_CARDS'
+  STORE_ERROR_STARSHIPS_CARDS = 'STORE_ERROR_STARSHIPS_CARDS'
 }
 
 export interface StarshipsCardsAction<Payload> extends Action<Payload> {}
--- src/store/starshipsCards/reducers/index.ts
+++ src/store/starshipsCards/reducers/index.ts
@@ -17,10 +17,5 @@ export const starshipsCardsReducer = handleActions<StarshipsCardsState>({
   [ACTION_TYPE.STORE_ERROR_STARSHIPS_CARDS]: (state) => ({
     ...state,
     error: "We had some trouble to connect with space galactic. Roll again"
-  }),
-  [ACTION_TYPE.CLEAR_STARSHIPS_CARDS]: () => ({
-    leftCard: undefined,
-    rightCard: undefined,
-    error: undefined
-  }),
+  })
 }, {...initialState})
