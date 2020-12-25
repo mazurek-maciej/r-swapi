0168f0426ede70ef395544069343ef2a2fdc1781
Maciej Mazurek <maciej.mazurek.prv@gmail.com>
2020-12-25 10:58:37 +0100

    Add test for handleOnClick in header component

--- src/components/Header/index.test.tsx
+++ src/components/Header/index.test.tsx
@@ -1,7 +1,8 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
 import Header from './'
 ;
 import { GameType } from "../../store/models/GameType"
+import userEvent from "@testing-library/user-event";
 
 describe('Header component', () => {
   const gameType = GameType.people;
@@ -32,5 +33,14 @@ describe('Header component', () => {
     const button = screen.getByText(/change cards to people/i);
 
     expect(button).toBeInTheDocument();
+  })
+
+  it('should call handleOnClick method after click switch card type button', async () => {
+    const userSelectedGameType = true;
+    render(<Header gameType={gameType} userSelectedGameType={userSelectedGameType} handleOnClick={handleOnClick} />)
+    const button = screen.getByText(/change cards to starships/i);
+
+    userEvent.click(button);
+    await waitFor(() => expect(handleOnClick).toBeCalledTimes(1))
   })
 })
