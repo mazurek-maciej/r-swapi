6c0660bebf013244dceccd1bf55136e9bd04ecb1
Maciej Mazurek <maciej.mazurek.prv@gmail.com>
2020-12-28 13:11:38 +0100

    Fix Header tests

--- src/components/Header/index.test.tsx
+++ src/components/Header/index.test.tsx
@@ -1,16 +1,14 @@
 import { render, screen, waitFor } from "@testing-library/react"
 import Header from './'
 ;
-import { GameType } from "../../store/models/GameType"
 import userEvent from "@testing-library/user-event";
 
 describe('Header component', () => {
-  const gameType = GameType.people;
   const userSelectedGameType = false;
   const handleOnClick = jest.fn();
 
   it('should render header element', () => {
-    render(<Header gameType={gameType} userSelectedGameType={userSelectedGameType} handleOnClick={handleOnClick} />)
+    render(<Header userSelectedGameType={userSelectedGameType} handleOnClick={handleOnClick} />)
     const title = screen.getByText('gwizdek');
     const selectGameType = screen.getByText('Choose cards type');
 
@@ -18,27 +16,10 @@ describe('Header component', () => {
     expect(selectGameType).toBeInTheDocument();
   })
 
-  it('should render switch to starships button when people cards are selected', () => {
-    const userSelectedGameType = true;
-    render(<Header gameType={gameType} userSelectedGameType={userSelectedGameType} handleOnClick={handleOnClick} />)
-    const button = screen.getByText(/change cards to starships/i);
-
-    expect(button).toBeInTheDocument();
-  })
-
-  it('should render switch to people button when starships cards are selected', () => {
-    const gameType = GameType.starships;
-    const userSelectedGameType = true;
-    render(<Header gameType={gameType} userSelectedGameType={userSelectedGameType} handleOnClick={handleOnClick} />)
-    const button = screen.getByText(/change cards to people/i);
-
-    expect(button).toBeInTheDocument();
-  })
-
   it('should call handleOnClick method after click switch card type button', async () => {
     const userSelectedGameType = true;
-    render(<Header gameType={gameType} userSelectedGameType={userSelectedGameType} handleOnClick={handleOnClick} />)
-    const button = screen.getByText(/change cards to starships/i);
+    render(<Header userSelectedGameType={userSelectedGameType} handleOnClick={handleOnClick} />)
+    const button = screen.getByText(/switch cards/i);
 
     userEvent.click(button);
     await waitFor(() => expect(handleOnClick).toBeCalledTimes(1))
